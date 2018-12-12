62e814b963fb5bdb32c2a15fc62bfa486f3ac64b
Ioannis Papadopoulos <ipap360@gmail.com>
2018-12-12 14:10:22 +0200

    restrict duplicate patient code

--- src/main/java/com/team360/hms/admissions/units/patients/PatientForm.java
+++ src/main/java/com/team360/hms/admissions/units/patients/PatientForm.java
@@ -4,6 +4,7 @@ import com.team360.hms.admissions.common.exceptions.FormValidationException;
 import lombok.Data;
 
 import java.util.HashMap;
+import java.util.Optional;
 
 @Data
 public class PatientForm {
@@ -22,12 +23,20 @@ public class PatientForm {
 
     PatientForm validate() {
         HashMap<String, String> errors = new HashMap();
+
         if (getName() == null) {
             errors.put("name", "Please fill the name");
         }
+
         if (getGender() == null) {
             errors.put("gender", "Please select the gender");
         }
+
+        Optional<Integer> exists = (new PatientDao()).checkCodeExists(getId(), getCode());
+        if (exists.isPresent()) {
+            errors.put("code", "A patient with the same code already exists");
+        }
+
         if (!errors.isEmpty()) {
             throw new FormValidationException(errors);
         }
